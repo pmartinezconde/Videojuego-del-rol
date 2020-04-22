d06625ebd981e9fd4e9d373bb0817f50247dd55a
pmartinezconde <61211218+pmartinezconde@users.noreply.github.com>
2020-04-22 18:50:42 +0200

    notas funcionamiento equipo

--- Codigo juego/Item.cpp
+++ Codigo juego/Item.cpp
@@ -6,5 +6,5 @@ typedef struct
 	int Attack;
 	int Speed;
 } Item;
-//hay que tener en cuenta que las pociones RECUPERAN salud, mientras que la armadura AUMENTA la salud o la velocidad según la que sea
+//hay que tener en cuenta que las pociones RECUPERAN salud, mientras que la armadura AUMENTA la salud
 //luego las armas aumentarán el daño de los ataques, en algún caso también la velocidad, estando relacionado tipo armas rapidas=menos daño, y armas pesadas=mas daño
