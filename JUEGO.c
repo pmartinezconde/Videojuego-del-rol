178ea77432a768571c6c20118a6cdafabebb8aec
pmartinezconde <61211218+pmartinezconde@users.noreply.github.com>
2020-05-06 21:46:52 +0200

    clases personaje

--- JUEGO.c
+++ JUEGO.c
@@ -19,18 +19,65 @@ int main(void)
 {
 Personaje pers;	
 char menu;
+int menunum;
 printf("MENU, ELIGE :\n 1-Empezar partida\n 2-Salir del juego\n");
 scanf("%c",&menu);
 switch(menu)
 {
     case '1':
     {
+    
     printf("Bienvenido\n");
     printf("Introduzca el nombre del personaje(10 caracteres maximo):\n");
     fflush(stdin);
     gets(pers.nombre);
+    
+    printf("Elige la clase de tu personaje: \n 1-Guerrero\n 2-Barbaro\n 3-Guardian\n 4-Caballero\n");
+    scanf("%i",&menunum);
+    {
+    	case 1:
+    		{
+    			printf("El guerrero ha sobrevivido multiples batallas, teniendo un cuerpo resistente\n");
+    			pers.atk=6;
+    			pers.hp=35;
+    			pers.def=6;
+    			
+    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+			}break;
+    	
+		case 2:
+			{
+				printf("El barbaro aprovecha su fuerza natural para realizar el maximo daño\n");
+				pers.atk=15;
+    			pers.hp=20;
+    			pers.def=6;
+    			
+    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+			}break;
+		case 3:	
+			{
+				pers.atk=6;
+    			pers.hp=20;
+    			pers.def=15;
+				printf("El guardian esta entrenado en el arte de la defensa, reduciendo las posibilidades de sufrir daño\n");
+				
+				printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+			}break;
+		case 4:
+			{
+				printf("El caballero ha entrenado en varias artes, siendo un personaje equilibrado\n");
+				pers.atk=10;
+    			pers.hp=20;
+    			pers.def=10;
+    			
+    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+			}break;
+	}
+    
     system("PAUSE");
     } break;
+    
+   
     case '2':
     {
     printf("¡Hasta la proxima!");
