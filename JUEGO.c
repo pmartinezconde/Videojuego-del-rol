e060c22011abbeda2d79ebebbe7a18b663f8df79
Miguel Ángel Pascual Collar <61047659+ClonTrolls@users.noreply.github.com>
2020-05-06 23:06:41 +0200

    Update JUEGO.c

--- JUEGO.c
+++ JUEGO.c
@@ -31,23 +31,21 @@ switch(menu)
     gets(pers.nombre);   
     printf("Elige la clase de tu personaje: \n 1-Guerrero\n 2-Barbaro\n 3-Guardian\n 4-Caballero\n\n-El guerrero ha sobrevivido multiples batallas, teniendo un cuerpo resistente\n-El barbaro aprovecha su fuerza natural para atacar a sus enemigos\n-El guardian esta entrenado en el arte de la defensa, reduciendo las posibilidades de sufrir daño\n-El caballero ha entrenado en varias artes, siendo un personaje equilibrado\n");
     scanf("%c",&menunum);
-    
     	if(menunum=='1')
     		{	
     			pers.atk=6;
     			pers.hp=35;
     			pers.def=6;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
-			}
-    	
+    			printf("Has elegido Guerrero Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+			}	
 		else if(menunum=='2')
 			{
 				pers.atk=15;
     			pers.hp=20;
     			pers.def=6;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+    			printf("Has elegido Barbaro Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
 		else if(menunum=='3')	
 			{
@@ -55,7 +53,7 @@ switch(menu)
     			pers.hp=20;
     			pers.def=15;
 				
-				printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+				printf("Has elegido Guardian Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
 		else if(menunum=='4')
 			{
@@ -63,10 +61,9 @@ switch(menu)
     			pers.hp=20;
     			pers.def=10;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+    			printf("Has elegido Caballero Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
-}break;
-    
+    }break; 
     case '2':
     {
     printf("¡Hasta la proxima!");
