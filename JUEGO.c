b99ea50953926e0014cac1fa2d7016e3195414ad
Miguel Ángel Pascual Collar <61047659+ClonTrolls@users.noreply.github.com>
2020-05-06 21:58:47 +0200

    Update JUEGO.c

--- JUEGO.c
+++ JUEGO.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #define LONG 10
@@ -19,25 +18,22 @@ typedef struct
 int main(void)
 {
 Personaje pers;	
-char menu;
-int menunum;
+char menu,menunum;
 printf("MENU, ELIGE :\n 1-Empezar partida\n 2-Salir del juego\n");
 scanf("%c",&menu);
 switch(menu)
 {
     case '1':
     {
-    
     printf("Bienvenido\n");
     printf("Introduzca el nombre del personaje(10 caracteres maximo):\n");
     fflush(stdin);
-    gets(pers.nombre);
-    
+    gets(pers.nombre);   
     printf("Elige la clase de tu personaje: \n 1-Guerrero\n 2-Barbaro\n 3-Guardian\n 4-Caballero\n");
-    scanf("%i",&menunum);
+    scanf("%c",&menunum);
     switch(menunum);
     {
-    	case 1:
+    	case '1':
     		{
     			printf("El guerrero ha sobrevivido multiples batallas, teniendo un cuerpo resistente\n");
     			pers.atk=6;
@@ -47,7 +43,7 @@ switch(menu)
     			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}break;
     	
-		case 2:
+		case '2':
 			{
 				printf("El barbaro aprovecha su fuerza natural para realizar el maximo daño\n");
 				pers.atk=15;
@@ -56,7 +52,7 @@ switch(menu)
     			
     			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}break;
-		case 3:	
+		case '3':	
 			{
 				pers.atk=6;
     			pers.hp=20;
@@ -65,7 +61,7 @@ switch(menu)
 				
 				printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}break;
-		case 4:
+		case '4':
 			{
 				printf("El caballero ha entrenado en varias artes, siendo un personaje equilibrado\n");
 				pers.atk=10;
@@ -75,11 +71,8 @@ switch(menu)
     			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}break;
 	}
-    
     system("PAUSE");
     } break;
-    
-   
     case '2':
     {
     printf("¡Hasta la proxima!");
