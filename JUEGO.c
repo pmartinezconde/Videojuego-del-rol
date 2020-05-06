0ad49926a73a65e133668ad64d1434e9a92fabe1
Miguel Ángel Pascual Collar <61047659+ClonTrolls@users.noreply.github.com>
2020-05-06 21:21:24 +0200

    Update JUEGO.c

--- JUEGO.c
+++ JUEGO.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-int main(void) 
+#define LONG 10
+typedef struct
 {
-	char menu;
+	int espada;
+	int armadura; 
+	int escudo;
+} Inventario;
+typedef struct
+{
+	char nombre[LONG];
+	int atk;
+	int hp; 
+	int def;
+	Inventario inv;
+} Personaje;
+int main(void)
+{
+Personaje pers;	
+char menu;
 printf("MENU, ELIGE :\n 1-Empezar partida\n 2-Salir del juego\n");
-	scanf("%c",&menu);
-	switch(menu)
-	{
-		case '1':
-			{
-				printf("Bienvenido");
-				break;
-			}
-		case '2':
-		printf("¡Hasta la proxima!");
-			break;
-		
-	default:
-		
-		break;
-
-	}
-	
-	return 0;
+scanf("%c",&menu);
+switch(menu)
+{
+    case '1':
+    {
+    printf("Bienvenido\n");
+    printf("Introduzca el nombre del personaje(10 caracteres maximo):\n");
+    fflush(stdin);
+    gets(pers.nombre);
+    system("PAUSE");
+    } break;
+    case '2':
+    {
+    printf("Â¡Hasta la proxima!");
+    } break;
+    default:
+    {
+    } break;
+}
+    return 0;
 }
