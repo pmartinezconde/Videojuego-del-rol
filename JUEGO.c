b5fc62b3b5e0f5dc91c51d9314c29774ba81ef7c
pmartinezconde <61211218+pmartinezconde@users.noreply.github.com>
2020-05-07 03:23:57 +0200

    menu basico, salir del juego, etc
    He añadido varias funciones, entre ellas una de menu y de escena para poder facilitar la organizacion del codigo, asi como una pregunta al morir de si quieres volver a jugar (aun falta implementar funcion de combate que desecadene en dato de muerte)

--- JUEGO.c
+++ JUEGO.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LONG 16
+typedef struct
+{
+	int decision;
+	int salida;
+}Menu;
 //Stats del inventario
 typedef struct
 {
 	int espada;
+	char nombespada;
 	int armadura; 
+	char nombarmadura;
 	int escudo;
+	char nombescudo;
 	int dinero;
 } Inventario;
 //Stats del personaje
@@ -27,12 +35,53 @@ int stats(int salud, int saludtemporal, int fuerza, int defensa, int dinero)
 	Inventario inv;
 	printf("|Salud: %i / %i  Fuerza: %i   Defensa: %i|  Dinero: %i", salud, saludtemporal, fuerza, defensa, dinero);
 }
+int escena(void)
+{
+int opcion;
+int pista;
+printf("\n\n Te despiertas en una cueva a oscuras...\n ¿Que haces?\n -1 Inspeccionar los alrededores\n -2 Buscar una salida\n-3 MENU\n");
+			
+		do{
+			
+			scanf("%i",&opcion);
+			if(opcion==1)
+			{
+			printf("A tus alrededores ves varias piedras que parecen haberse desprendido  hoyo que hay en el techo, del cual deduces que has caido\n");
+			pista=1;
+			}
+			if(opcion==2)
+			{
+			printf("Ves al frente como la cueva continua, ");
+			if (pista==1)
+			printf(" y no parece que puedas escalar para salir por donde caiste");
+			printf(", por lo que decides avanzar por la cueva\n");
+			}
+
+			}while(opcion!=3);
+menuopciones();
+}
+int menuopciones(void)
+{
+	Menu options;
+	int sel;
+	system("cls");
+	printf("-1: Volver\n-2 Usar objetos\n-3 Salir del juego ");
+	scanf("%i",&sel);
+	if(sel==1)
+	escena();
+	if(sel==2)
+	//inven(); funcion inventario
+	if(sel==3)
+	exit(0);
+	
+}
 int main(void)
 {
+Menu options;
 Inventario inv;
 Personaje pers;	
 char menu,menunum;
-int rehacer;
+int rehacer, pista, muerte=0;
 do
 {
 system("cls");
@@ -56,7 +105,7 @@ switch(menu)
     			pers.hpactual=35;
     			pers.def=6;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+    			printf("Has elegido al Guerrero\nAtaque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
     	
 		else if(menunum=='2')
@@ -66,7 +115,7 @@ switch(menu)
     			pers.hpactual=20;
     			pers.def=6;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+    			printf("Has elegido al Barbaro\nAtaque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
 		else if(menunum=='3')	
 			{
@@ -75,7 +124,7 @@ switch(menu)
     			pers.hpactual=20;
     			pers.def=15;
 				
-				printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+				printf("Has elegido al Guardian\nAtaque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
 		else if(menunum=='4')
 			{
@@ -84,26 +133,45 @@ switch(menu)
     			pers.hpactual=27;
     			pers.def=10;
     			
-    			printf("Ataque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
+    			printf("Has elegido al Caballero\nAtaque: %i   Salud: %i    Defensa: %i\n",pers.atk,pers.hp,pers.def);
 			}
 	printf("-1 Comenzar aventura\n-2 Volver al menu para cambiar personaje\n");
 	scanf("%i",&rehacer);
 	if(rehacer==1)
-	stats(pers.hp, pers.hpactual,pers.atk,pers.def,inv.dinero);
+		{	
+			inv.dinero=0;
+			stats(pers.hp, pers.hpactual,pers.atk,pers.def,inv.dinero);
+			
+			escena();
+			if(options.salida==0)
+			exit(0);
+		}
+			while(0<menunum<5);
+		
+		}
 	
-	
-}break;
+	break;
     
     case '2':
     {
     printf("¡Hasta la proxima!");
+    exit(0);
     } break;
     default:
-    {
+    
+    {	
     } break;
 }
 }
     while(rehacer==2);
+    if(muerte=1)
+    {
+	printf("Has muerto, quieres volver a empezar?\n -1: Volver al menu\n -2: Salir del juego\n");
+    
+    	scanf("%i",&menunum);
+    	if(menunum==1)
+    	main();
+    }
 
 return 0;
 }
